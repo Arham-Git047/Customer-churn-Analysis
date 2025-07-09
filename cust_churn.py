d0cd22bce343b0cd069f8d4a48be50f4b5806002
Arham-Git047 <arhamkelkar15@gmail.com>
2025-07-09 16:54:34 +0530

    Add files via upload

---
+++ cust_churn.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+df = pd.read_csv("C://Users//admin//Documents//Cust_Churn DS//Telco_Customer_Churn_Dataset  (3).csv")
+
+
+print(df.shape)         
+print(df.columns)       
+print(df.head(2))
