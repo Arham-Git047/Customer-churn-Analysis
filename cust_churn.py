e0547f75dd9da6ed6f9324fb978292092e01d83a
Arham-Git047 <arhamkelkar15@gmail.com>
2025-08-26 18:29:58 +0530

    Delete cust_churn.py
    another file got uploaded

--- cust_churn.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("C://Users//admin//Documents//Cust_Churn DS//Telco_Customer_Churn_Dataset  (3).csv")
-
-
-print(df.shape)         
-print(df.columns)       
-print(df.head(2))
